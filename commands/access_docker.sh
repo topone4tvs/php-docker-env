969e297abea3959db0c80ff3fd96bfa143145ed7
huasheng <huasheng@in66.com>
2017-09-18 16:54:54 +0800

    update env

--- commands/access_docker.sh
+++ commands/access_docker.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-docker_containers=(devdocker_php-fpm_1 devdocker_nginx_1 devdocker_workspace_1 devdocker_memcached_1 devdocker_mysql_1)
-container_flag=${docker_containers[0]}
-
-container_name=$1
-if [ "$container_name" = "php" ]; then
-    container_flag=${docker_containers[0]}
-elif [ "$container_name" = "nginx" ]; then
-    container_flag=${docker_containers[1]}
-elif [ "$container_name" = "workspace" ]; then
-    container_flag=${docker_containers[2]}
-elif [ "$container_name" = "memcached" ]; then
-    container_flag=${docker_containers[3]}
-elif [ "$container_name" = "mysql" ]; then
-    container_flag=${docker_containers[4]}
-fi
+container_flag=$(docker-compose --project-directory=/var/env/dev_docker ps | grep $1 | grep -v 'grep' | awk '{print $1}')
 echo $container_flag
 sudo docker exec -it $container_flag sh
