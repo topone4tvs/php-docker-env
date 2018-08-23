109f1f8578a48f449a63cb0b269145ee8e5a5127
huasheng <huasheng@in66.com>
2018-08-23 14:39:13 +0800

    update pro

--- commands/access_docker.sh
+++ commands/access_docker.sh
@@ -2,4 +2,4 @@
 
 container_flag=$(docker-compose --project-directory=/var/env/dev_docker ps | grep $1 | grep -v 'grep' | awk '{print $1}')
 echo $container_flag
-sudo docker exec -it $container_flag sh
+sudo docker exec -it $container_flag bash
